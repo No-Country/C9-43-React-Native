f66d0a258be9450c416ad792cf5e13e23f64cdac
Edisonmunoz13 <horacioem595>
2023-02-13 18:48:50 -0300

    fix LoginScreen

--- React-Native/React-Native-Front/src/screens/LoginScreen.jsx
+++ React-Native/React-Native-Front/src/screens/LoginScreen.jsx
@@ -1,157 +1,160 @@
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { AntDesign, Ionicons } from '@expo/vector-icons';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
 
 // TODO: Ver logo y bordes inputs
 
 export const LoginScreen = ({ navigation }) => {
-
-    const [secured, setSecured] = useState(true);
+  const [secured, setSecured] = useState(true);
 
   return (
-    <View style={ styles.container }>
+    <View style={styles.container}>
+      <AntDesign
+        name="arrowleft"
+        size={24}
+        color="black"
+        style={styles.arrow}
+        onPress={() => navigation.navigate("MainScreen")}
+      />
 
-        <AntDesign 
-            name="arrowleft" 
-            size={24} 
-            color="black" 
-            style={ styles.arrow }
-            onPress={ () => navigation.navigate('MainScreen') }
+      <Image style={styles.logo} source={require("../../assets/logo.png")} />
+
+      <Text style={styles.title}>Bienvenido a House Quest!</Text>
+
+      <Text style={styles.subtitle}>Ingresar con mi E-Mail</Text>
+
+      <View style={styles.inputs}>
+        <TextInput
+          style={[styles.emailInput, styles.textInputs]}
+          variant="outlined"
+          placeholder="Ingresa tu E-mail"
+          placeholderTextColor={"#979797"}
+          inputContainerStyle={{ backgroundColor: "#E4E4E4" }}
         />
 
-        <Image
-            style={ styles.logo } 
-            source={ require('../../assets/logo.png') }
-        />
-
-        <Text style={ styles.title }>
-            Bienvenido a House Quest!
-        </Text>
-
-        <Text style={ styles.subtitle }>
-            Ingresar con mi e-Mail
-        </Text>
-
-        <View style={ styles.inputs }>
-
-            <TextInput 
-                style={ styles.emailInput }
-                variant='outlined'
-                placeholder='Ingresa tu E-mail'
-                placeholderTextColor={'#414141'}
-                inputContainerStyle={{ backgroundColor: '#D9D9D9' }}
-            />
-
-            <View style={ styles.passwordInputView }>
-                <TextInput 
-                    style={ styles.passwordInput }
-                    secureTextEntry={ secured }
-                    autoCapitalize='none'
-                    autoComplete='off'
-                    autoCorrect={ false }
-                    placeholder='Ingresa tu contraseña'
-                    placeholderTextColor={ '#414141' }
-                />
-                <Ionicons 
-                    style={ styles.passwordInputIcon }
-                    name={ secured ? 'eye-outline' : 'eye-off-outline' }
-                    size={24} 
-                    color="black"
-                    onPress={ () => setSecured( prev => !prev )}
-                />
-            </View>
-
-            <TouchableOpacity
-                style={ styles.joinButton }
-            >
-                <Text style={ styles.joinButtonText }>
-                    Ingresar
-                </Text>
-            </TouchableOpacity>
-
+        <View style={styles.passwordInputView}>
+          <TextInput
+            style={[styles.passwordInput, styles.textInputs]}
+            secureTextEntry={secured}
+            autoCapitalize="none"
+            autoComplete="off"
+            autoCorrect={false}
+            placeholder="Ingresa tu contraseña"
+            placeholderTextColor={"#979797"}
+          />
+          <Ionicons
+            style={styles.passwordInputIcon}
+            name={secured ? "eye-outline" : "eye-off-outline"}
+            size={24}
+            color="black"
+            onPress={() => setSecured((prev) => !prev)}
+          />
         </View>
 
+        <TouchableOpacity>
+          <View style={styles.joinButton}>
+            <Text style={styles.joinButtonText}>Ingresar</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#FFFFFF'
-    },
-    arrow: {
-        alignSelf: 'flex-start',
-        marginTop: 46,
-        left: 27
-    },
-    logo: {
-        marginTop: 40,
-        width: 89,
-        height: 66
-    },
-    title: {
-        fontWeight: '600',
-        fontSize: 20,
-        marginTop: 32
-    },
-    subtitle: {
-        marginTop: 11,
-        fontWeight: '300',
-        fontSize: 15
-    },
-    inputs: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 45,
-        width: '100%',
-        paddingHorizontal: 16
-    },
-    emailInput: {
-        width: '100%',
-        height: 56,
-        paddingLeft: 16,
-        borderWidth: 1,
-        borderRadius: 4,
-        borderColor: '#414141',
-        backgroundColor: '#E4E4E4',
-        fontSize: 16,
-        color: '#414141'
-    },
-    passwordInputView: {
-        width: '100%',
-        flexDirection: 'row',
-        height: 56,
-        marginTop: 20,
-        alignItems: 'center',
-        backgroundColor: '#E4E4E4',
-        borderWidth: 1,
-        borderRadius: 4,
-        borderColor: '#414141'
-    },
-    passwordInput: {
-        flex: 1,
-        height: 56,
-        paddingLeft: 16,
-        fontSize: 16,
-        color: '#414141'
-    },
-    passwordInputIcon: {
-        marginHorizontal: 20,
-    },
-    joinButton: {
-        marginTop: 73,
-        width: 160,
-        height: 49,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 4,
-        backgroundColor: '#000000'
-    },
-    joinButtonText: {
-        color: '#FFFFFF',
-        fontWeight: '500',
-        letterSpacing: 1.25
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  arrow: {
+    alignSelf: "flex-start",
+    marginTop: 46,
+    left: 27,
+  },
+  logo: {
+    marginTop: 40,
+    width: 89,
+    height: 66,
+  },
+  title: {
+    fontWeight: "500",
+    fontSize: 19,
+    marginTop: 32,
+  },
+  subtitle: {
+    marginTop: 11,
+    fontWeight: "400",
+    fontSize: 16,
+  },
+  inputs: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 45,
+    width: 328,
+    paddingHorizontal: 16,
+  },
+  textInputs: {
+    fontSize: 16,
+    fontWeight: "400",
+  },
+
+  emailInput: {
+    width: 328,
+    height: 56,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#fff",
+    backgroundColor: "#E4E4E4",
+    color: "#414141",
+    paddingLeft: 16,
+  },
+  passwordInputView: {
+    width: 328,
+    flexDirection: "row",
+    height: 56,
+    marginTop: 20,
+    alignItems: "center",
+    backgroundColor: "#E4E4E4",
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#fff",
+  },
+  passwordInput: {
+    flex: 1,
+    height: 56,
+    color: "#414141",
+    paddingLeft: 16,
+  },
+  passwordInputIcon: {
+    marginHorizontal: 20,
+  },
+  joinButton: {
+    backgroundColor: "#018349",
+    marginTop: 30,
+    width: 328,
+    height: 49,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    marginTop: 50,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  joinButtonText: {
+    color: "#FFFFFF",
+    fontWeight: "500",
+    letterSpacing: 1.25,
+    fontSize: 14,
+  },
+});
--- React-Native/React-Native-Front/src/screens/RegisterScreen.jsx
+++ React-Native/React-Native-Front/src/screens/RegisterScreen.jsx
@@ -5,53 +5,87 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  ScrollView,
 } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import { useState } from "react";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
 
 export const RegisterScreen = ({ navigation }) => {
+  const [secured, setSecured] = useState(true);
+  const [securedConfirm, setSecuredConfirm] = useState(true);
+
   return (
     <>
-      <View style={styles.container}>
-        <AntDesign
-          name="arrowleft"
-          size={24}
-          color="black"
-          style={styles.arrow}
-          onPress={() => navigation.navigate("MainScreen")}
-        />
-        <Image style={styles.logo} source={require("../../assets/logo.png")} />
-
-        <Text style={styles.title}>Ingresá tus datos</Text>
-
-        <View style={[styles.inputs]}>
-          <TextInput
-            style={[styles.emailInput, styles.textInputs]}
-            placeholder="Ingresa tu E-mail"
-            placeholderTextColor="#979797"
+      <ScrollView>
+        <View style={styles.container}>
+          <AntDesign
+            name="arrowleft"
+            size={24}
+            color="black"
+            style={styles.arrow}
+            onPress={() => navigation.navigate("MainScreen")}
+          />
+          <Image
+            style={styles.logo}
+            source={require("../../assets/logo.png")}
           />
 
-          <TextInput
-            style={[styles.passwordInput, styles.textInputs]}
-            placeholder="Ingresa tu contraseña"
-            placeholderTextColor="#979797"
-          />
+          <Text style={styles.title}>Ingresá tus datos</Text>
 
-          <TextInput
-            style={[styles.passwordInput, styles.textInputs]}
-            placeholder="Volvé a ingresar tu contraseña"
-            placeholderTextColor="#979797"
-          />
+          <View style={[styles.inputs]}>
+            <TextInput
+              style={[styles.emailInput, styles.textInputs]}
+              placeholder="Ingresa tu E-mail"
+              placeholderTextColor="#979797"
+            />
 
-          <TouchableOpacity style={styles.registerButton}>
-            <Text style={styles.registerButtonText}>Registrarme</Text>
-          </TouchableOpacity>
+            <View style={[styles.passwordInput, styles.textInputs]}>
+              <TextInput
+                placeholder="Ingresa tu contraseña"
+                placeholderTextColor="#979797"
+                secureTextEntry={secured}
+              />
+
+              <Ionicons
+                style={styles.passwordInputIcon}
+                name={secured ? "eye-outline" : "eye-off-outline"}
+                size={24}
+                color="black"
+                onPress={() => setSecured((prev) => !prev)}
+              />
+            </View>
+            <View style={[styles.passwordInput, styles.textInputs]}>
+              <TextInput
+                placeholder="Volvé a ingresar tu contraseña"
+                placeholderTextColor="#979797"
+                secureTextEntry={securedConfirm}
+              />
+              <Ionicons
+                style={styles.passwordInputIcon}
+                name={securedConfirm ? "eye-outline" : "eye-off-outline"}
+                size={24}
+                color="black"
+                onPress={() => setSecuredConfirm((prev) => !prev)}
+              />
+            </View>
+
+            <TouchableOpacity>
+              <View style={styles.registerButton}>
+                <Text style={styles.registerButtonText}>Registrarme</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  passwordInputIcon: {
+    paddingTop: 15,
+  },
+
   container: {
     flex: 1,
     alignItems: "center",
@@ -78,6 +112,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "400",
     marginBottom: -10,
+    width: 328,
+    height: 56,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingEnd: 16,
   },
   emailInput: {
     width: 326,
@@ -98,8 +137,6 @@ const styles = StyleSheet.create({
   },
   passwordInput: {
     marginTop: 30,
-    width: 326,
-    height: 47,
     backgroundColor: "#D9D9D9",
     paddingLeft: 19,
     borderRadius: 5,
@@ -111,7 +148,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.22,
     shadowRadius: 2.22,
 
-    elevation: 3,
+    elevation: 0,
   },
   registerButton: {
     backgroundColor: "#018349",
@@ -122,6 +159,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 4,
     marginTop: 50,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
   },
   registerButtonText: {
     fontWeight: "500",
