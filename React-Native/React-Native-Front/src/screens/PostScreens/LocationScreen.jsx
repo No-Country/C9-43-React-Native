ce12169aa2e9e2e3a594990d34bd7343928c39bc
RichardLoz <lozanorichard32@gmail.com>
2023-03-01 01:34:40 -0300

    Avanzando con los filtros y modificacion de componentes

--- React-Native/React-Native-Front/src/screens/PostScreens/LocationScreen.jsx
+++ React-Native/React-Native-Front/src/screens/PostScreens/LocationScreen.jsx
@@ -1,80 +1,91 @@
-import { useContext } from "react"
-import { Pressable, StyleSheet, Text, View } from "react-native"
-import { GreenButton, PostInputs } from "../../components"
-import { IconHeader } from "../../components/layout"
-import { PublishPostContext } from "../../context/publish-post-context/PublishPostContext"
-import { PublishProgressContext } from "../../context/publish-progress-context/PublishProgressContext"
+import { useContext } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { GreenButton, PostInputs } from "../../components";
+import { IconHeader } from "../../components/layout";
+import { PublishPostContext } from "../../context/publish-post-context/PublishPostContext";
+import { PublishProgressContext } from "../../context/publish-progress-context/PublishProgressContext";
 
 export const LocationScreen = ({ navigation }) => {
-  const { publishPost, handlePublishPost } = useContext(PublishPostContext)
-  const { handlePublishProgress } = useContext(PublishProgressContext)
+  const { publishPost, handlePublishPost } = useContext(PublishPostContext);
+  const { handlePublishProgress } = useContext(PublishProgressContext);
 
-  console.log(publishPost)
+  const MIN_DESCRIPTION_LENGTH = 20;
 
+  console.log(publishPost);
 
   const handleInputs = (name, input) => {
-    handlePublishPost(name, input)
-  }
+    handlePublishPost(name, input);
+  };
 
   const handleAccept = () => {
-    if ( publishPost.city.length === 0 || publishPost.region.length === 0) {
-      alert('No dejes campos vacios')
-      return
-    } 
-    handlePublishProgress('location', 10)
-    navigation.goBack()
-  }
+    if (publishPost.city.length === 0 || publishPost.region.length === 0) {
+      alert("No dejes campos vacios");
+      return;
+    }
+    // Validacion de caracteres minimos (20)
+    if (
+      publishPost.city.length < MIN_DESCRIPTION_LENGTH ||
+      publishPost.region.length < MIN_DESCRIPTION_LENGTH
+    ) {
+      alert(
+        `La descripción debe tener al menos ${MIN_DESCRIPTION_LENGTH} caracteres`
+      );
+      return;
+    }
+    handlePublishProgress("location", 10);
+    navigation.goBack();
+  };
 
   return (
-    <View style={ styles.container }>
+    <View style={styles.container}>
+      <IconHeader
+        icon={"close"}
+        title={"Publicar anuncio"}
+        navigation={navigation}
+      />
+      <View style={styles.descriptionInputContainer}>
+        <Text style={styles.title}>Ubicación</Text>
 
-        <IconHeader icon={'close'} title={'Publicar anuncio'} navigation={navigation}/>
-        <View style={styles.descriptionInputContainer}>
-        <Text style={ styles.title }>
-            Ubicación
-        </Text>
-
-        <PostInputs 
-          titleAndPlaceholder={[ 
-            { 
-              title: 'Indicá en que ciudad está ubicado', 
-              placeholder: 'Ej: Quilmes',
-              name: 'city'
+        <PostInputs
+          titleAndPlaceholder={[
+            {
+              title: "Indicá en que ciudad está ubicado",
+              placeholder: "Ej: Quilmes",
+              name: "city",
             },
             {
-              title: 'Indicá en que provincia está ubicado',
-              placeholder: 'Ej: Buenos Aires',
-              name: 'region'
-            }
+              title: "Indicá en que provincia está ubicado",
+              placeholder: "Ej: Buenos Aires",
+              name: "region",
+            },
           ]}
           handleInputs={handleInputs}
           publishPost={publishPost}
         />
-        </View>
-        <Pressable style={styles.buttonContainer} onPress={handleAccept}>
-          <GreenButton text={'Aceptar'} />
-        </Pressable>
-
+      </View>
+      <Pressable style={styles.buttonContainer} onPress={handleAccept}>
+        <GreenButton text={"Aceptar"} />
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF'
-    },
-    title: {
-      fontSize: 26,
-      lineHeight: 32,
-      marginLeft: 19,
-      marginTop: 22
-    },
-    descriptionInputContainer: {
-      flex: 7
-    },
-    buttonContainer: {
-      flex: 1,
-      marginHorizontal: 16
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  title: {
+    fontSize: 26,
+    lineHeight: 32,
+    marginLeft: 19,
+    marginTop: 22,
+  },
+  descriptionInputContainer: {
+    flex: 7,
+  },
+  buttonContainer: {
+    flex: 1,
+    marginHorizontal: 16,
+  },
+});
